866bb05b8404b5a7baf93676c520f0c0c8a04dcc
Chocotonto <145541262+Chocotonto@users.noreply.github.com>
2024-06-07 13:43:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from jugador import Player
+from game import Game
+
+p1 = Player(1, 'a')
+p2 = Player(2, 'b')
+
+g1 = Game(p1, p2, 3)
+
+print(f"p1: {p1.toString()}")
+print(f"p2: {p2.toString()}")
+
+g1.play()
+
+print(f"El ganador mediante g1.winner().toString() es: {g1.winner().toString()}")
